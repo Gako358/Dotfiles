305faa9b011c231815a7c4fadf602c3051533480
Gako358 <gako358@outlook.com>
2020-11-07 15:14:42 +0100

    auto updated

---
+++ Config/fish/functions/pygit.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function pygit --wraps='~/Projects/Scheme/pygit/main.py' --wraps='~/Projects/Scheme/pygit/main.py' --description 'alias pygit ~/Projects/Scheme/pygit/main.py'
+  ~/Projects/Scheme/pygit/main.py $argv;
+end
