e4fbc49e0e35c33dfd35d73cd43a1dd099d4837c
Gako358 <gako358@outlook.com>
2020-11-11 22:38:45 +0100

    updated fish alias

--- Config/fish/functions/pygit.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function pygit --wraps='~/Projects/Scheme/pygit/main.py' --wraps='~/Projects/Scheme/pygit/main.py' --description 'alias pygit ~/Projects/Scheme/pygit/main.py'
-  ~/Projects/Scheme/pygit/main.py $argv;
-end
